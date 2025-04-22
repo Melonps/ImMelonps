c4853c35121b9153de55986acedc625cc3522c95
Melonps <kake.kake0311@gmail.com>
2025-04-22 22:03:47 +0900

    [FEAT] コンテンツの追加

--- app/routes/_renderer.tsx
+++ app/routes/_renderer.tsx
@@ -1,12 +1,11 @@
 import { jsxRenderer } from "hono/jsx-renderer";
 
-export default jsxRenderer(({ children, title }) => {
+export default jsxRenderer(({ children }) => {
   return (
     <html lang="en">
       <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        {<title>{title}</title>}
         <link rel="stylesheet" href="/static/style.css" />
       </head>
       <body>
